1c2787a21606a76ae5462781b57d44b720002e9e
mayadaazzh <mayadaazizah27@gmail.com>
2022-09-21 19:50:04 +0700

    commit pertama

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char karakter;
+    printf("masukkan karakter: ");
+    karakter = getchar();
+    printf("hasil karakter: %c\n",karakter);
+    return 0;
+}
